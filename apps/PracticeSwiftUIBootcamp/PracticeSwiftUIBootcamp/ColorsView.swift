742a8cebf8169bf3a222c26bbd42b236b6591c72
Raghuvamsi Ayapilla <vamsi.ar@gmail.com>
2025-11-12 21:33:46 -0500

    Add ColorsView and enhance ShapesView with a Rectangle example and shadow effect for improved visual presentation.

---
+++ apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/ColorsView.swift
@@ -0,0 +1,18 @@
+//
+//  ColorsView.swift
+//  PracticeSwiftUIBootcamp
+//
+//  Created by raghuvamsi ayapilla on 11/12/25.
+//
+
+import SwiftUI
+
+struct ColorsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorsView()
+}
--- apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/ShapesView.swift
+++ apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/ShapesView.swift
@@ -1,1 +1,25 @@
+import SwiftUI
 
+struct ShapesView: View {
+    var body: some View {
+//        Circle()
+//        Ellipse()
+//        Capsule(style: .circular)
+        Rectangle()
+            .fill(Color.blue)
+//            .foregroundColor(.green)
+//            .stroke()
+//            .stroke(Color.red)
+//            .stroke(Color.blue, lineWidth: 10)
+//            .stroke(Color.orange, style: StrokeStyle(lineWidth: 30, lineCap: .round, dash: [50]))
+//            .trim(from: 0.4, to: 1.0)
+//            .stroke(Color.purple)
+//            .strokeBorder(Color.red)
+            .frame(width: 300, height: 200)
+            .shadow(radius: 50)
+    }
+}
+
+#Preview {
+    ShapesView()
+}
