6c81c53f479b336156f2d2da2f21de27f23ba83b
Raghuvamsi Ayapilla <vamsi.ar@gmail.com>
2025-11-17 20:52:19 -0500

    Add color selection functionality to ColorsView and introduce a new color asset for enhanced visual customization.

--- apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/ColorsView.swift
+++ apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/ColorsView.swift
@@ -1,15 +1,18 @@
-//
-//  ColorsView.swift
-//  PracticeSwiftUIBootcamp
-//
-//  Created by raghuvamsi ayapilla on 11/12/25.
-//
-
 import SwiftUI
 
 struct ColorsView: View {
+    @State private var selectedColor = Color.green
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ColorPicker("Pick a color", selection: $selectedColor)
+            Rectangle()
+                .fill(selectedColor)
+                .frame(width: 300, height: 250)
+                .shadow(color: Color(red: 0.50, green: 0.50, blue: 0.50).opacity(0.8),
+                        radius: 10.00, x: 0.00, y: 0.00)
+        }
+        .padding()
     }
 }
 
