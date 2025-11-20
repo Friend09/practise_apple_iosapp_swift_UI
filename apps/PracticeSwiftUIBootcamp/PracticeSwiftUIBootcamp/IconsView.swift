b9e4eff27230b872446eacafa92f31822aa43ce8
Raghuvamsi Ayapilla <vamsi.ar@gmail.com>
2025-11-19 21:00:50 -0500

    Update IconsView to display a heart icon and add new ImageView component for future enhancements.

--- apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/IconsView.swift
+++ apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/IconsView.swift
@@ -2,7 +2,18 @@ import SwiftUI
 
 struct IconsView: View {
     var body: some View {
-        Text("Hello")
+        Image(systemName: "heart.fill")
+//            .renderingMode(.original)
+            .font(.largeTitle)
+//            .resizable()
+//            .aspectRatio(contentMode: .fit)
+            .scaledToFit()
+            //.scaledToFill()
+            .font(.caption)
+//            .font(.system(size: 2000))
+            .foregroundColor(Color(#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)))
+//            .frame(width: 400, height: 400)
+            .clipped()
     }
 }
 
---
+++ apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/ImageView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageView.swift
+//  PracticeSwiftUIBootcamp
+//
+//  Created by raghuvamsi ayapilla on 11/19/25.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+    var body: some View {
+        Text("ImageView")
+    }
+}
+
+#Preview {
+    ImageView()
+}
