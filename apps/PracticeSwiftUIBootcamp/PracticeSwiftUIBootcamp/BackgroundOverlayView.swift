77e94b5eb41f21afae1b16dbd28cbfbd7e47ed2b
Raghuvamsi Ayapilla <vamsi.ar@gmail.com>
2025-11-24 21:23:50 -0500

    Add BackgroundOverlayView component and enhance FrameView with multiple background colors and frame adjustments for improved layout and visual appeal.

---
+++ apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/BackgroundOverlayView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct BackgroundOverlayView: View {
+    var body: some View {
+        Text("Overlays")
+    }
+}
+
+#Preview {
+    BackgroundOverlayView()
+}
--- apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/FrameView.swift
+++ apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/FrameView.swift
@@ -10,6 +10,17 @@ import SwiftUI
 struct FrameView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .background(Color.red)
+            .frame(height: 100, alignment: .top)
+            .background(Color.orange)
+            .frame(width: 150)
+            .background(Color.purple)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(Color.pink)
+            .frame(height: 400)
+            .background(Color.green)
+            .frame(maxHeight: .infinity, alignment: .top)
+            .background(Color.yellow)
     }
 }
 
