fd33e655399deab46947fa9130ed06779c2e33c4
Raghuvamsi Ayapilla <vamsi.ar@gmail.com>
2025-11-26 07:21:18 -0500

    Add BackgroundOverlayView with a heart icon and gradient background, and introduce StacksView as a new component for future development.

---
+++ apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/StacksView.swift
@@ -0,0 +1,18 @@
+//
+//  StacksView.swift
+//  PracticeSwiftUIBootcamp
+//
+//  Created by raghuvamsi ayapilla on 11/26/25.
+//
+
+import SwiftUI
+
+struct StacksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StacksView()
+}
