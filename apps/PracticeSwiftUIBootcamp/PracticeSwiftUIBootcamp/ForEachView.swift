1c8d11290860d72953debcf5e413136f393a1532
Raghuvamsi Ayapilla <vamsi.ar@gmail.com>
2025-12-05 07:26:45 -0500

    Add ForEachView component and enhance InitializerView with dynamic properties for fruit selection and improved UI layout.

---
+++ apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/ForEachView.swift
@@ -0,0 +1,18 @@
+//
+//  ForEachView.swift
+//  PracticeSwiftUIBootcamp
+//
+//  Created by raghuvamsi ayapilla on 12/5/25.
+//
+
+import SwiftUI
+
+struct ForEachView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForEachView()
+}
--- apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/InitializerView.swift
+++ apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/InitializerView.swift
@@ -1,18 +1,50 @@
-//
-//  InitializerView.swift
-//  PracticeSwiftUIBootcamp
-//
-//  Created by raghuvamsi ayapilla on 12/3/25.
-//
-
 import SwiftUI
 
 struct InitializerView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+    let backgroundColor: Color
+    let count: Int
+    let title: String
+    
+    init(count: Int, fruit: Fruit) {
+        self.count = count
+        
+        if fruit == .apple {
+            self.title = "Apples"
+            self.backgroundColor = .red
+        } else {
+            self.title = "Oranges"
+            self.backgroundColor = .orange
+        }
     }
+    
+    enum Fruit {
+        case apple
+        case orange
+    }
+    
+    var body: some View {
+        VStack(spacing: 12) {
+            Text("\(count)")
+                .font(.largeTitle)
+                .foregroundStyle(.white)
+                .underline()
+            
+            Text("title")
+                .font(.headline)
+                .foregroundStyle(.white)
+        }
+        .frame(width: 150, height: 150)
+        .background(backgroundColor)
+        .cornerRadius(10)
+        .shadow(radius: 5)
+    }
+    
 }
 
 #Preview {
-    InitializerView()
+    HStack {
+        InitializerView(count: 5, fruit: .apple)
+        InitializerView(count: 25, fruit: .orange)
+    }
 }
