4e391621a57c99cf5c39dbe75a90ec4d3effc8e8
Raghuvamsi Ayapilla <vamsi.ar@gmail.com>
2025-12-11 05:27:04 -0500

    Add MyGridView with a LazyVGrid layout and sections, and introduce SafeAreaView as a new component for future use.

---
+++ apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/SafeAreaView.swift
@@ -0,0 +1,18 @@
+//
+//  SafeAreaView.swift
+//  PracticeSwiftUIBootcamp
+//
+//  Created by raghuvamsi ayapilla on 12/11/25.
+//
+
+import SwiftUI
+
+struct SafeAreaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SafeAreaView()
+}
