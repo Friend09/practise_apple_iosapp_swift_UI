4f42dd70847a476799bf5317d9980b2f3b2d63af
Raghuvamsi Ayapilla <vamsi.ar@gmail.com>
2025-12-15 20:42:00 -0500

    Add ButtonsView component and enhance SafeAreaView with a ScrollView and dynamic content layout for improved UI presentation.

---
+++ apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/ButtonsView.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonsView.swift
+//  PracticeSwiftUIBootcamp
+//
+//  Created by raghuvamsi ayapilla on 12/15/25.
+//
+
+import SwiftUI
+
+struct ButtonsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonsView()
+}
--- apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/SafeAreaView.swift
+++ apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/SafeAreaView.swift
@@ -1,15 +1,27 @@
-//
-//  SafeAreaView.swift
-//  PracticeSwiftUIBootcamp
-//
-//  Created by raghuvamsi ayapilla on 12/11/25.
-//
-
 import SwiftUI
 
 struct SafeAreaView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        ScrollView {
+            VStack {
+                Text("Title goes Here")
+                    .font(.largeTitle)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                ForEach(0..<10) { index in
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .fill(Color.white)
+                        .frame(height: 150)
+                        .shadow(radius: 10)
+                        .padding(20)
+                }
+            }
+        }
+        .background(
+            Color.red
+                .ignoresSafeArea(edges: .top)
+        )
     }
 }
 
