8324a82f974e07cd3dbcfe6ca9e0e5ddf8a27567
Raghuvamsi Ayapilla <vamsi.ar@gmail.com>
2025-12-16 20:50:21 -0500

    Add ButtonsView with multiple interactive buttons and a dynamic title update feature. Introduce StateView as a new component for future use.

--- apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/ButtonsView.swift
+++ apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/ButtonsView.swift
@@ -1,15 +1,67 @@
-//
-//  ButtonsView.swift
-//  PracticeSwiftUIBootcamp
-//
-//  Created by raghuvamsi ayapilla on 12/15/25.
-//
-
 import SwiftUI
 
 struct ButtonsView: View {
+    
+    @State var title: String = "This is my title"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text(title)
+            
+            // button 1
+            Button("Press me!") {
+                self.title = "Button was Pressed!"
+            }
+            .accentColor(.red)
+            
+            // button 2
+            Button {
+                self.title = "Button #2 was pressed"
+            } label: {
+                Text("Save".uppercased())
+                    .font(.headline)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .padding()
+                    .padding(.horizontal, 20)
+                    .background(
+                        Color.blue
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                    )
+            }
+            
+            Button {
+                self.title = "Button #3"
+            } label: {
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 75, height: 75)
+                    .shadow(radius: 10)
+                    .overlay(
+                        Image(systemName: "heart.fill")
+                            .font(.largeTitle)
+                            .foregroundColor(Color(#colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)))
+                    )
+            }
+            
+            Button {
+                self.title = "Button #4"
+            } label: {
+                Text("Finish".uppercased())
+                    .font(.caption)
+                    .bold()
+                    .foregroundStyle(.gray)
+                    .padding()
+                    .padding(.horizontal, 10)
+                    .background(
+                        Capsule()
+                            .stroke(Color.gray, lineWidth: 2.0)
+                    )
+            }
+
+            
+        }
     }
 }
 
---
+++ apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/StateView.swift
@@ -0,0 +1,18 @@
+//
+//  StateView.swift
+//  PracticeSwiftUIBootcamp
+//
+//  Created by raghuvamsi ayapilla on 12/16/25.
+//
+
+import SwiftUI
+
+struct StateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StateView()
+}
