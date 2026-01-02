e32c6a86d74bda78ac02f6bdd0ca508114b47aa8
Raghuvamsi Ayapilla <vamsi.ar@gmail.com>
2026-01-02 08:55:53 -0500

    Rename StateView to StateBootcamp and implement interactive buttons to change background color, update title, and manage a count variable. Enhance UI with a dynamic layout in SwiftUI.

--- apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/StateView.swift
+++ apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/StateView.swift
@@ -1,18 +1,42 @@
-//
-//  StateView.swift
-//  PracticeSwiftUIBootcamp
-//
-//  Created by raghuvamsi ayapilla on 12/16/25.
-//
-
 import SwiftUI
 
-struct StateView: View {
+struct StateBootcamp: View {
+    @State var backgroundColor: Color = Color.green
+    @State var myTitle: String = "My Title"
+    @State var count: Int = 0
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // background
+            backgroundColor.ignoresSafeArea(.all)
+            
+            // content
+            VStack(spacing: 20) {
+                Text(myTitle)
+                    .font(.title)
+                Text("Count: \(count)")
+                    .font(.headline)
+                    .underline()
+                
+                HStack(spacing: 20) {
+                    Button("BUTTON 1") {
+                        backgroundColor = .red
+                        myTitle = "BUTTON 1 was pressed"
+                        count += 1
+                    }
+                    Button("BUTTON 2") {
+                        backgroundColor = .purple
+                        myTitle = "BUTTON 2 was pressed"
+                        count -= 1
+                    }
+                }
+            }
+            .foregroundStyle(.white)
+        }
     }
 }
 
 #Preview {
-    StateView()
+    StateBootcamp()
 }
