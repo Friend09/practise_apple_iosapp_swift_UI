f4e6da19baca82dd7cb8b3a88cfe870fa6803f5e
Raghuvamsi Ayapilla <vamsi.ar@gmail.com>
2026-02-02 21:29:54 -0500

    Add AlertView and ListView components to enhance UI with a grocery list feature. Introduce interactive elements for managing fruits and veggies, including add, delete, and move functionalities.

--- apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/ListView.swift
+++ apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/ListView.swift
@@ -1,1 +1,81 @@
+//
+//  ListBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Nick Sarno on 1/28/21.
+//
 
+import SwiftUI
+
+struct ListView: View {
+    
+    @State var fruits: [String] = [
+        "apple", "orange", "banana", "peach"
+    ]
+    @State var veggies: [String] = [
+        "tomato", "potato", "carrot"
+    ]
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section(
+                    header:
+                        HStack {
+                            Text("Fruits")
+                            Image(systemName: "flame.fill")
+                        }
+                        .font(.headline)
+                        .foregroundColor(.orange)
+                ) {
+                    ForEach(fruits, id: \.self) { fruit in
+                        Text(fruit.capitalized)
+                            .font(.caption)
+                            .foregroundColor(.white)
+                            .padding(.vertical)
+                    }
+                    .onDelete(perform: delete)
+                    .onMove(perform: move)
+                    .listRowBackground(Color.blue)
+                }
+                
+                Section(header: Text("Veggies")) {
+                    ForEach(veggies, id: \.self) { veggies in
+                        Text(veggies.capitalized)
+                    }
+                }
+            }
+            .accentColor(.purple)
+            //.listStyle(SidebarListStyle())
+            .navigationTitle("Grocery List")
+            .navigationBarItems(leading: EditButton(), trailing: addButton)
+        }
+        .accentColor(.red)
+    }
+    
+    var addButton: some View {
+        Button("Add", action: {
+            add()
+        })
+    }
+    
+    func delete(indexSet: IndexSet) {
+        fruits.remove(atOffsets: indexSet)
+    }
+    
+    func move(indices: IndexSet, newOffset: Int) {
+        fruits.move(fromOffsets: indices, toOffset: newOffset)
+    }
+    
+    func add() {
+        fruits.append("Coconut")
+    }
+}
+
+#Preview {
+    ListView(fruits: [
+        "apple", "orange", "banana", "peach"
+    ], veggies: [
+        "tomato", "potato", "carrot"
+    ])
+}
