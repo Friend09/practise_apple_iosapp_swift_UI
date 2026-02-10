64aa2489a8503319f7ec430e6d7f00bca1118374
Raghuvamsi Ayapilla <vamsi.ar@gmail.com>
2026-02-09 19:43:54 -0500

    Add ActionsheetView and AlertView components to enhance UI with alert functionality. Introduce interactive buttons for triggering alerts and managing background color based on alert type.

--- apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/AlertView.swift
+++ apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/AlertView.swift
@@ -1,1 +1,49 @@
+import SwiftUI
 
+struct AlertView: View {
+    @State var showAlert: Bool = false
+    @State var alertType: MyAlerts? = nil
+    @State var backgroundColor: Color = .yellow
+    
+    enum MyAlerts {
+        case success
+        case error
+    }
+    
+    var body: some View {
+        ZStack {
+            backgroundColor.ignoresSafeArea(.all)
+            
+            VStack {
+                Button("Button 1") {
+                    alertType = .error
+                    showAlert.toggle()
+                }
+                Button("Button 2") {
+                    alertType = .success
+                    showAlert.toggle()
+                }
+            }
+            .alert(isPresented: $showAlert) {
+                getAlert()
+            }
+        }
+    }
+    
+    func getAlert() -> Alert {
+        switch alertType {
+        case .success:
+            return Alert(title: Text("This was a success!"),  message: nil, dismissButton: .default(Text("OK"), action: {
+                backgroundColor = .green
+            }))
+        case .error:
+            return Alert(title: Text("There was an error!"))
+        case nil:
+            return Alert(title: Text("This is from nil"))
+        }
+    }
+}
+
+#Preview {
+    AlertView(showAlert: false, alertType: nil, backgroundColor: Color.yellow)
+}
