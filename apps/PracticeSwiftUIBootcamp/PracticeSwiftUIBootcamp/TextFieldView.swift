71fb9452c0eb522be725609877916506705bcab0
Raghuvamsi Ayapilla <vamsi.ar@gmail.com>
2026-02-25 05:32:34 -0500

    Add TextEditorView and enhance TextFieldView with interactive text input and save functionality

--- apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/TextFieldView.swift
+++ apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/TextFieldView.swift
@@ -1,1 +1,59 @@
+import SwiftUI
 
+struct TextFieldView: View {
+    
+    @State var textFieldText: String = ""
+    @State var dataArray: [String] = []
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField("Type something here...", text: $textFieldText)
+                //                .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .background(Color.gray.opacity(0.3).clipShape(.buttonBorder))
+                    .foregroundStyle(.red)
+                    .font(.headline)
+                
+                Button {
+                    if textIsAppropriate() {
+                        saveText()
+                    }
+                } label: {
+                    Text("save".uppercased())
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(textIsAppropriate() ? Color.blue : Color.gray)
+                        .clipShape(.buttonBorder)
+                        .foregroundStyle(.white)
+                        .font(.headline)
+                }
+                .disabled(!textIsAppropriate())
+                
+                ForEach(dataArray, id: \.self) { data in
+                    Text(data)
+                }
+                Spacer()
+            }
+        }
+        .padding()
+        .navigationTitle("TextField Bootcamp!")
+    }
+    
+    func textIsAppropriate() -> Bool {
+        // check text
+        if  textFieldText.count >= 3 {
+            return true
+        }
+        return false
+    }
+    
+    func  saveText() {
+        dataArray.append(textFieldText)
+        textFieldText = ""
+    }
+}
+
+#Preview {
+    TextFieldView()
+}
