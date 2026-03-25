9bcf5609803c424d0c785ca7610ec6aa3b77407e
Raghuvamsi Ayapilla <vamsi.ar@gmail.com>
2026-03-24 20:50:43 -0400

    Add DatePickerView component to enhance UI with date selection functionality. Introduce interactive elements for user date input.

--- apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/DatePickerView.swift
+++ apps/PracticeSwiftUIBootcamp/PracticeSwiftUIBootcamp/DatePickerView.swift
@@ -1,1 +1,31 @@
+import SwiftUI
 
+struct DatePickerView: View {
+    
+    @State var selectedDate: Date = Date()
+    let startingDate: Date = Calendar.current.date(from: DateComponents(year:2018)) ?? Date()
+    let endingDate: Date = Date()
+    
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }
+    
+    var body: some View {
+        VStack {
+            Text("SELECTED DATE IS:")
+            Text(dateFormatter.string(from: selectedDate)).font(.title)
+//            DatePicker("Select a Date", selection: $selectedDate)
+//            DatePicker("Select date", selection: $selectedDate, displayedComponents: [.date, .hourAndMinute])
+            DatePicker("Select a date", selection: $selectedDate, in: startingDate...endingDate, displayedComponents: [.date])
+                .tint(Color.red)
+                .datePickerStyle(CompactDatePickerStyle())
+        }
+    }
+}
+
+#Preview {
+    DatePickerView()
+}
